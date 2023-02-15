f730223f922217079223a511ce0e3b883042c388
JS <0324son@gmail.com>
2023-02-15 00:28:53 +0000

    add lidar data

---
+++ lidar/dataset_for_centercrop_lidar.py
@@ -0,0 +1,240 @@
+import json
+import os
+import torch
+import numpy as np
+import cv2
+import albumentations
+import albumentations.pytorch
+import radiate
+# from torchvision.io import read_image
+
+class Radiate_Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_folder, train_mode, transform = False):
+        self.root_dir = data_folder
+        self.folders = []
+        self.transform = transform
+        # print(len(os.listdir(self.root_dir)))
+        self.train_mode = train_mode
+        for curr_dir in os.listdir(self.root_dir):
+            with open(os.path.join(self.root_dir, curr_dir, 'meta.json')) as f:
+                meta = json.load(f)
+            if meta["set"] == train_mode:
+                self.folders.append(curr_dir)
+        self.folders.sort()
+        # print(self.folders)
+        # print(len(self.folders))
+        
+        self.radar_dicts = self.get_radar_dicts(self.folders)
+        # print(len(self.radar_dicts))
+        # self.datas = []
+        self.data_idx = 0        
+        # for idx, _ in enumerate(self.radar_dicts):
+        #     record_init = {}
+        #     record_init["data_idx"] = self.data_idx
+        #     record_init["img"] = self.radar_dicts[idx]
+        #     record_init["train_mode"] = self.train_mode
+        #     self.data_idx += 1
+        #     self.datas.append(record_init)
+        
+    def __len__(self):
+        return len(self.radar_dicts)
+    
+    def __getitem__(self, idx):
+        radar_dict = self.radar_dicts[idx]
+        img = cv2.imread(radar_dict["file_name"])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # print(radar_dict["img"])
+        boxes = []
+        boxes_angle = []
+        bbox_OOD = []
+        category_id = []
+        angle = []
+        target = {}
+        img_info = {}
+        num_objs = 0
+        difficulties = []
+
+        for i in range(len(radar_dict['annotations'])):
+            boxes.append(radar_dict['annotations'][i]['bbox'])
+            boxes_angle.append(radar_dict['annotations'][i]['bbox_angle'])
+            bbox_OOD.append(radar_dict['annotations'][i]['bbox_OOD'])
+            category_id.append(radar_dict['annotations'][i]['category_id'])
+            difficulties.append(['0'])
+            num_objs += 1
+        
+        boxes = torch.as_tensor(boxes, dtype = torch.float32)
+        angle = torch.as_tensor(angle, dtype = torch.float32)
+        bbox_OOD = torch.as_tensor(bbox_OOD, dtype = torch.float32)
+        category_id = torch.as_tensor(category_id, dtype = torch.float32)
+        iscrowd = torch.zeros((num_objs,), dtype = torch.int64)
+    
+        target["file_name"] = radar_dict['file_name']
+        target["frame_number"] = radar_dict['frame_number']
+        target["folder_name"] = radar_dict['folder_name']
+        target["bboxes"] = boxes
+        target["bboxes_angle"] = boxes_angle
+        target["bboxes_OOD"] = bbox_OOD
+        target["category_id"] = category_id
+        target['image_id'] = radar_dict['image_id']
+        # target["iscrowd"] = iscrowd 
+        target["difficulty"] = difficulties
+        # labels = category_id
+        img_info["folder_name"] = radar_dict['folder_name']
+        img_info["frame_number"] = radar_dict['frame_number']
+        
+        radiate_sdk = radiate.Sequence(os.path.join(self.root_dir, target["folder_name"]), config_file='config/config.yaml')
+        timestamp = np.float64(radiate_sdk.timestamp_radar['time'][int(target['frame_number'][:-4])-1])
+        output = radiate_sdk.get_from_timestamp(timestamp, get_annotations=False)
+        lidar_img = output['sensors']['lidar_bev_image']
+        if self.transform:
+            data_transform = albumentations.Compose([
+                albumentations.CenterCrop(256,256)],
+                # albumentations.pytorch.transforms.ToTensorV2()],
+                bbox_params=albumentations.BboxParams(format='pascal_voc', label_fields=['labels']),
+                )
+            data_transform_yolo = albumentations.Compose([
+                albumentations.CenterCrop(256,256)],
+                # albumentations.pytorch.transforms.ToTensorV2()],
+                bbox_params=albumentations.BboxParams(format='yolo', label_fields=['labels']),
+                )
+            data_transform_lidar = albumentations.Compose([
+                albumentations.CenterCrop(256,256)])
+
+            transformed = data_transform(image = img, bboxes = target['bboxes'], labels = target["category_id"])
+            transformed_2 = data_transform_yolo(image = img, bboxes = target['bboxes_angle'], labels = target["category_id"])
+            transformed_3 = data_transform_lidar(image = lidar_img)
+            img = transformed['image']
+            target['bboxes'] = torch.as_tensor(transformed['bboxes'], dtype=torch.float32)
+            target['category_id'] = torch.as_tensor(transformed['labels'], dtype=torch.float32)
+            target['bboxes_angle'] = torch.as_tensor(transformed_2['bboxes'], dtype=torch.float32)
+            lidar_img = transformed_3['image']
+            # labels = transformed['labels']
+        
+        return img, lidar_img, target, img_info
+    
+
+
+    def gen_boundingbox_OBB(self, bbox, angle):
+        theta = np.deg2rad(-angle)
+        R = np.array([[np.cos(theta), -np.sin(theta)],
+                      [np.sin(theta), np.cos(theta)]])
+        points = np.array([[bbox[0], bbox[1]],
+                           [bbox[0] + bbox[2], bbox[1]],
+                           [bbox[0] + bbox[2], bbox[1] + bbox[3]],
+                           [bbox[0], bbox[1] + bbox[3]]]).T
+
+        cx = bbox[0] + bbox[2] / 2
+        cy = bbox[1] + bbox[3] / 2
+        T = np.array([[cx], [cy]])
+
+        points = points - T
+        points = np.matmul(R, points) + T
+        points = points.astype(float)
+        
+        return points
+
+    def gen_boundingbox_HBB(self, bbox, angle):
+        theta = np.deg2rad(-angle)
+        R = np.array([[np.cos(theta), -np.sin(theta)],
+                      [np.sin(theta), np.cos(theta)]])
+        points = np.array([[bbox[0], bbox[1]],
+                           [bbox[0] + bbox[2], bbox[1]],
+                           [bbox[0] + bbox[2], bbox[1] + bbox[3]],
+                           [bbox[0], bbox[1] + bbox[3]]]).T
+
+        cx = bbox[0] + bbox[2] / 2
+        cy = bbox[1] + bbox[3] / 2
+        T = np.array([[cx], [cy]])
+
+        points = points - T
+        points = np.matmul(R, points) + T
+        points = points.astype(float)
+
+        min_x = np.min(points[0, :])
+        min_y = np.min(points[1, :])
+        max_x = np.max(points[0, :])
+        max_y = np.max(points[1, :])
+
+        return min_x, min_y, max_x, max_y
+    
+    def get_radar_dicts(self, folders):
+        dataset_dicts = []
+        idd = 0
+        folder_size = len(folders)
+        file_lenght = 0
+        for idx, folder in enumerate(folders):
+            # radiate_sdk = radiate.Sequence(os.path.join(self.root_dir, folder), config_file='config/config.yaml')
+            radar_folder = os.path.join(self.root_dir, folder, 'Navtech_Cartesian')
+            annotation_path = os.path.join(self.root_dir,
+                                           folder, 'annotations', 'annotations.json')
+            with open(annotation_path, 'r') as f_annotation:
+                annotation = json.load(f_annotation)
+
+            folder_number = idx
+            radar_files = os.listdir(radar_folder)
+            radar_files.sort()
+            # print(radar_files)
+            
+            for frame_number in range(len(radar_files)):
+                # if folder_number == 0:
+                #     print(frame_number)
+                record = {}
+                objs = []
+                bb_created = True
+                idd += 1
+                filename = os.path.join(
+                    radar_folder, radar_files[frame_number])
+                # print(filename)
+                if (not os.path.isfile(filename)):
+                    print(filename)
+                    continue
+                record["folder_name"] = folder
+                record["frame_number"] = radar_files[frame_number]
+                record["file_name"] = filename
+                record["image_id"] = idd-1
+                record["height"] = 1152
+                record["width"] = 1152
+                record["folder_number"] = folder_number
+
+                for object in annotation:
+                    if (object['bboxes'][frame_number]):
+                        class_obj = object['class_name']
+                        if (class_obj != 'pedestrian' and class_obj != 'group_of_pedestrians'):
+                            bbox = object['bboxes'][frame_number]['position']
+                            angle = object['bboxes'][frame_number]['rotation']
+                            # bb_created = True
+                            # if cfg.MODEL.PROPOSAL_GENERATOR.NAME == "RRPN":
+                            min_x, min_y, max_x, max_y = self.gen_boundingbox_HBB(bbox, angle)
+
+                            # print(bbox, angle.shape)
+                            points = self.gen_boundingbox_OBB(bbox, angle)
+                            x1 = points[0][0]
+                            y1 = points[1][0]
+                            x2 = points[0][1]
+                            y2 = points[1][1]
+                            x3 = points[0][2]
+                            y3 = points[1][2]
+                            x4 = points[0][3]
+                            y4 = points[1][3]
+                            
+                            
+                            cx = (bbox[0] + bbox[2] / 2) / 1152
+                            cy = (bbox[1] + bbox[3] / 2) / 1152
+                            wid = (bbox[2]) / 1152
+                            hei = (bbox[3]) / 1152
+                            
+                            obj = {
+                                "bbox": [min_x, min_y, max_x, max_y],
+                                "bbox_angle": [cx, cy, wid, hei, angle],
+                                "bbox_OOD": [x1, y1, x2, y2, x3, y3, x4, y4],
+                                "category_id": 0,
+                                "iscrowd": 0
+                            }
+
+                            objs.append(obj)
+                # if bb_created:
+                record["annotations"] = objs
+                dataset_dicts.append(record)
+        # print(dataset_dicts)
+        return dataset_dicts
+    
---
+++ lidar/make_lidar_gbw.py
@@ -0,0 +1,169 @@
+import json
+import os
+import torch
+import numpy as np
+import cv2
+import albumentations
+import albumentations.pytorch
+# from torchvision.io import read_image
+
+import argparse
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from dataset_for_centercrop_lidar import Radiate_Dataset
+
+import PIL
+import numpy as np
+import cv2
+import shutil
+from tqdm import tqdm
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_folder", help="root folder with radiate dataset", 
+                    default='/workspace/dataset/radiate', type=str)
+
+parser.add_argument("--save_folder", help="root for save directory",
+                    default = "/workspace/JS/faster_rcnn_JS/checkpoint", type=str)
+
+parser.add_argument("--train_mode", help="dataset mode ('train_good_weather', 'train_good_and_bad_weather', 'test')",
+                    default='train_good_and_bad_weather', type=str)
+
+
+
+
+args = parser.parse_args()
+data_folder = args.data_folder
+train_mode = args.train_mode
+
+def gen_boundingbox(bbox, angle):
+    theta = np.deg2rad(-angle)
+    R = np.array([[np.cos(theta), -np.sin(theta)],
+                    [np.sin(theta), np.cos(theta)]])
+    
+    x1 = bbox[0] - bbox[2] / 2
+    x2 = bbox[0] + bbox[2] / 2
+    y1 = bbox[1] - bbox[3] / 2
+    y2 = bbox[1] + bbox[3] / 2
+    
+    points = np.array([[x1, y1],
+                       [x2, y1],
+                       [x2, y2],
+                       [x1, y2]]).T
+
+    cx = bbox[0]
+    cy = bbox[1]
+
+    T = np.array([[cx], [cy]])
+
+    points = points - T
+    points = np.matmul(R, points) + T
+    points = points.astype(float)
+    
+    return points
+
+def main():
+
+    root_dir = '/workspace/dataset/radiate_lidar'
+    dataset = Radiate_Dataset(data_folder, train_mode, transform=True)
+
+    img_radar_save = os.path.join(root_dir, train_mode, 'images_radar')
+    img_lidar_save = os.path.join(root_dir, train_mode, 'images_lidar')
+    img_add_save = os.path.join(root_dir, train_mode, 'images_add')
+    
+    label_save = os.path.join(root_dir, train_mode, 'labels')
+    label_angle_save = os.path.join(root_dir, train_mode, 'labels_angle')
+    label_OBB_save = os.path.join(root_dir, train_mode, 'labelTxt')
+    img_info_save = os.path.join(root_dir, train_mode, 'img_info')
+    os.makedirs(img_radar_save, exist_ok=True)
+    os.makedirs(img_lidar_save, exist_ok=True)
+    os.makedirs(img_add_save, exist_ok=True)
+    os.makedirs(label_save, exist_ok=True)
+    os.makedirs(label_angle_save, exist_ok=True)
+    os.makedirs(label_OBB_save, exist_ok=True)
+    os.makedirs(img_info_save, exist_ok=True)
+
+    for i in tqdm(range(len(dataset))):
+        img_radar, img_lidar, target, img_info = dataset[i]
+        img_add = img_radar+img_lidar
+        file_name = target['file_name']
+        bbox = target['bboxes']
+        bbox_angle = target['bboxes_angle']
+        category = target['category_id']
+        img_radar_save_path = os.path.join(img_radar_save, str(i).zfill(5)+'.png')
+        img_lidar_save_path = os.path.join(img_lidar_save, str(i).zfill(5)+'.png')
+        img_add_save_path = os.path.join(img_add_save, str(i).zfill(5)+'.png')
+        label_save_path = os.path.join(label_save, str(i).zfill(5)+'.txt')
+        label_angle_save_path = os.path.join(label_angle_save, str(i).zfill(5)+'.txt')
+        label_OBB_save_path = os.path.join(label_OBB_save, str(i).zfill(5)+'.txt')
+        img_info_save_path = os.path.join(img_info_save, str(i).zfill(5)+'.json')
+        
+        # print(bbox_angle)
+        # print(img_save_path)
+        cv2.imwrite(img_radar_save_path, img_radar)
+        cv2.imwrite(img_lidar_save_path, img_lidar)
+        cv2.imwrite(img_add_save_path, img_add)
+        
+        # save HBB (cx, cy, wid, hei)
+        with open(label_save_path, 'w') as f:
+            for j in range(bbox.shape[0]):
+                # print("%i %.6f %.6f %.6f %.6f" % (category[j], bbox[j][0], bbox[j][1], bbox[j][2], bbox[j][3]))
+                # print(str(np.array(category[j]).astype(np.int32)))
+                cx = (bbox[j][0] + bbox[j][2]) / 2
+                cy = (bbox[j][1] + bbox[j][3]) / 2
+                wid = bbox[j][2] - bbox[j][0]
+                hei = bbox[j][3] - bbox[j][1]
+                f.write("%i %.6f %.6f %.6f %.6f \n" % (category[j], cx, cy, wid, hei))
+                
+        # save OBB(cx, cy, wid, hei, angle)
+        with open(label_angle_save_path,"w") as f:
+            for j in range(bbox_angle.shape[0]):
+                # print("%i %.6f %.6f %.6f %.6f" % (category[j], bbox[j][0], bbox[j][1], bbox[j][2], bbox[j][3]))
+                # print(str(np.array(category[j]).astype(np.int32)))
+                cx = bbox_angle[j][0]*img_radar.shape[0]
+                cy = bbox_angle[j][1]*img_radar.shape[0]
+                wid = bbox_angle[j][2]*img_radar.shape[0]
+                hei = bbox_angle[j][3]*img_radar.shape[0]
+                angle = bbox_angle[j][4]
+                f.write("%.6f %.6f %.6f %.6f %.6f vehicle %i \n" % (cx, cy, wid, hei, angle, 0))
+        
+        with open(label_OBB_save_path,"w") as f:
+            bbox_OBB = []
+            for k, HBB in enumerate(bbox_angle):
+                points = gen_boundingbox(HBB*img_radar.shape[0], HBB[4])
+                x1 = points[0][0]
+                y1 = points[1][0]
+                x2 = points[0][1]
+                y2 = points[1][1]
+                x3 = points[0][2]
+                y3 = points[1][2]
+                x4 = points[0][3]
+                y4 = points[1][3]
+                if x1 < 0:
+                    x1 = 0
+                if x2 < 0:
+                    x2 = 0
+                if x3 < 0:
+                    x3 = 0
+                if x4 < 0:
+                    x4 = 0
+                if y1 < 0:
+                    y1 = 0
+                if y2 < 0:
+                    y2 = 0
+                if y3 < 0:
+                    y3 = 0
+                if y4 < 0:
+                    y4 = 0
+                bbox_OBB.append([x1, y1, x2, y2, x3, y3, x4, y4])
+            for j in range(bbox_angle.shape[0]):
+                # print("%i %.6f %.6f %.6f %.6f" % (category[j], bbox[j][0], bbox[j][1], bbox[j][2], bbox[j][3]))
+                # print(str(np.array(category[j]).astype(np.int32)))
+                f.write("%.1f %.1f %.1f %.1f %.1f %.1f %.1f %.1f vehicle %i \n" % (bbox_OBB[j][0], bbox_OBB[j][1], bbox_OBB[j][2], bbox_OBB[j][3], bbox_OBB[j][4], bbox_OBB[j][5], bbox_OBB[j][6], bbox_OBB[j][7], 0))
+        
+        with open(img_info_save_path, 'w', encoding='utf-8') as f:
+            json.dump(img_info, f)
+
+        
+    
+    
+if __name__ == '__main__':
+    main()
